523604f4e3e99fd31a40299c724b63945a447763
Karol Romeiro <anakromeiro@gmail.com>
2021-02-25 22:03:35 +0000

    Refactored app.py, added new extensions, implemented resouces and models. - Added Flask-RESTful extension - Added new files to handle recipe model and REST requests to add, edit, delete and publish/unpublished recipes - Refactor of app.py using flask_restful

--- app.py
+++ app.py
@@ -1,59 +1,14 @@
-from flask import Flask, jsonify, request
-from http import HTTPStatus
+from flask import Flask
+from flask_restful import Api
+from resources.recipe import RecipeListResource, RecipeResource, RecipePublishResource
 
 
 app = Flask(__name__)
+api = Api(app)
 
-recipes = [
-    {
-        'id': 1,
-        'name': 'Egg Salad',
-        'description': 'This is a lovely egg salad recipe.'
-    },
-    {
-        'id': 2, 'name': 'Tomato Pasta',
-        'description': 'This is a lovely tomato pasta recipe.'
-    }
-]
-
-@app.route('/recipes/', methods=['GET'])
-def get_recipes():
-    return jsonify({'data': recipes})
-
-@app.route('/recipe/<int:recipe_id>', methods=['GET'])
-def get_recipe(recipe_id):
-    recipe = next((recipe for recipe in recipes if recipe['id'] == recipe_id), None)
-    if recipe:
-        return jsonify(recipe)
-    return jsonify({'message': 'recipe not found'}), HTTPStatus.NOT_FOUND
-
-@app.route('/recipe', methods=['POST'])
-def create_recipe():
-    data = request.get_json()
-    name = data.get('name')
-    description = data.get('description')
-    recipe = {
-        'id': len(recipes) + 1,
-        'name': name,
-        'description': description
-    }
-    recipes.append(recipe)
-    return jsonify(recipe), HTTPStatus.CREATED
-
-@app.route('/recipe/<int:recipe_id>', methods=['PUT'])
-def update_recipe(recipe_id):
-    recipe = next((recipe for recipe in recipes if recipe['id'] == recipe_id), None)
-    if not recipe:
-        return jsonify({'message': 'recipe not found'}), HTTPStatus.NOT_FOUND
-    data = request.get_json()
-    recipe.update(
-        {
-            'name': data.get('name'),
-            'description': data.get('description')
-        }
-    )
-    return jsonify(recipe)
-
+api.add_resource(RecipeListResource, '/recipes')
+api.add_resource(RecipeResource, '/recipe/<int:recipe_id>')
+api.add_resource(RecipePublishResource, '/recipe/<int:recipe_id>/publish')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=5000, debug=True)
